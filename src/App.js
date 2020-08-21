546c4906a4613458f3f5c627c74f22142b7521fe
joaojesus81 <joao.jesus@arup.com>
2020-08-21 12:47:56 +0100

    remove option added

--- src/App.js
+++ src/App.js
@@ -16,29 +16,53 @@ class App extends React.Component {
         dueDate: "2020-08-24",
         priority: "high",
       },
+      {
+        description: "water the plants",
+        dueDate: "2020-10-24",
+        priority: "low",
+      },
     ],
   };
 
   render = () => {
-    console.log(this.state.listItems)
     return (
       <div className="App">
         <header>
           <h1>To Do List</h1>
         </header>
         <main>
-          <AddTask addNewTask={this.addNewTask}/>
-          <ToDoList listItems={this.state.listItems} />
+          <AddTask addNewTask={this.addNewTask} />
+          <ToDoList
+            listItems={this.state.listItems}
+            removeTask={this.removeTask}
+          />
         </main>
       </div>
     );
   };
 
   addNewTask = (newTask) => {
-    this.setState((currentState)=>{
-      return {listItems: [...currentState.listItems, {description: newTask.newDescription, dueDate: newTask.newDueDate, priority: newTask.newPriority}]}
-    })
-  }
+    this.setState((currentState) => {
+      return {
+        listItems: [
+          ...currentState.listItems,
+          {
+            description: newTask.newDescription,
+            dueDate: newTask.newDueDate,
+            priority: newTask.newPriority,
+          },
+        ],
+      };
+    });
+  };
+
+  removeTask = (indexToRemove) => {
+    this.setState(({ listItems }) => {
+      const copyArray = [...listItems];
+      copyArray.splice(indexToRemove, 1);
+      return { listItems: copyArray };
+    });
+  };
 }
 
 export default App;
--- src/components/to-do-list.jsx
+++ src/components/to-do-list.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 
 function ToDoList(props) {
+  const onClick = (event) => {
+    const targetTask = event.target.parentNode.id;
+    const targetIndex = props.listItems.findIndex((task) => {
+      return task.description === targetTask;
+    });
+    props.removeTask(targetIndex);
+  };
   return (
     <ul>
       {props.listItems.map((task) => {
         return (
-          <li key={task.description} className="container">
+          <li
+            key={task.description}
+            className="container"
+            id={task.description}
+          >
             <p>{task.description}</p>
             <p>{task.dueDate}</p>
             <p>{task.priority}</p>
+            <button onClick={onClick}>Delete Note!</button>
           </li>
         );
       })}
